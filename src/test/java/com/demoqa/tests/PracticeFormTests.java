94b68dae56a1ab4abe56140ad1c887a9950bf854
nyash <alex-nyashin1996@yandex.ru>
2022-10-10 00:22:20 +0300

    add config tests

--- src/test/java/com/demoqa/tests/PracticeFormTests.java
+++ src/test/java/com/demoqa/tests/PracticeFormTests.java
@@ -1,12 +1,15 @@
 package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.demoqa.helper.Attach;
 import com.demoqa.pages.PracticeFormPage;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
@@ -17,8 +20,18 @@ public class PracticeFormTests {
     PracticeFormPage practiceFormPage = new PracticeFormPage();
     @BeforeAll
     static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("browserName", "chrome");
+//        capabilities.setCapability("browserVersion", "100.0");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
     @Test
     void fillPracticeForm() {
